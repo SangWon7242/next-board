3303006ab0ba56489b0fd82b341c235ea4330eea
SangWon7242 <gotkdqja@gmail.com>
2025-12-11 01:24:57 +0900

    work

--- src/app/actions/post.ts
+++ src/app/actions/post.ts
@@ -1,28 +1,38 @@
-import { Board } from "@/app/types/Board";
-import { supabase } from "@/app/supabaseClient";
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { Post } from "@/app/types/post";
 
 // 보드 리스트
-export async function getAllBoards(): Promise<Board[] | null> {
+export async function getPosts(): Promise<Post[] | null> {
+  const supabase = await createClient();
+
   const { data, error } = await supabase
-    .from("board")
+    .from("posts")
     .select("*")
     .order("created_at", { ascending: false });
+
   if (error) {
     console.error("Error fetching boards:", error);
     return null;
   }
+
   return data;
 }
 
-export async function getBoardById(id: number): Promise<Board | null> {
+export async function getPostById(id: number): Promise<Post | null> {
+  const supabase = await createClient();
+
   const { data, error } = await supabase
-    .from("board")
+    .from("posts")
     .select("*")
     .eq("id", id)
     .single();
+
   if (error) {
     console.error("Error fetching board by ID:", error);
     return null;
   }
+
   return data;
 }
---
+++ src/app/actions/todo.ts
@@ -0,0 +1,19 @@
+import { createClient } from "@/lib/supabase/server";
+import { Todo } from "@/app/types/todo";
+
+// 모든 투두 가져오기
+export async function getTodos(): Promise<Todo[]> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("todos")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  return data || [];
+}
